5dc9551fad8654b142d8a24416f27e26a1f56582
Riccardo Sepe <seric814@gmail.com>
2024-01-29 15:54:33 +0100

    Reward makes sense

--- env/soft_reacher/soft_reacher.py
+++ env/soft_reacher/soft_reacher.py
@@ -21,7 +21,7 @@ class SoftReacher(BaseEnv):
         E = 1e3
         mu = 5e2
         d_diag = [1e-5, 1e-2, 1e-2]
-        self.eps = 1e-2
+        self._eps = 1e-2
 
         super(SoftReacher, self).__init__(name="soft_reacher",
                                           n=3,
@@ -42,6 +42,17 @@ class SoftReacher(BaseEnv):
         self.qdot_limit = np.array([600, 40, 20])
         assert self.qdot_limit.shape == (self.n,)
 
+        self._goal = None
+        self._sample_goal()
+
+    @property
+    def eps(self):
+        if np.abs(self.state[0]) < self._eps:
+            eps = self._eps * np.sign(self.state[0])
+        else:
+            eps = 0
+
+        return eps
 
     def reset_state(self):
         initial_pos = np.random.uniform(low=-0.01, high=0.01, size=self.n)
@@ -52,15 +63,17 @@ class SoftReacher(BaseEnv):
 
     def get_reward(self):
         q, qdot = self.state[:self.n], self.state[self.n:]
+        ee_pos = self.chi([0, self.r, self.eps, *q], self.l)[:2]
+        goal_dist = np.linalg.norm(ee_pos - self._goal)
 
-        pos_reward = rewards.tolerance(q, margin=self.l).min()
+        pos_reward = rewards.tolerance(goal_dist, margin=self.l)
         pos_reward = (1 + pos_reward) / 2
 
         vel_reward = rewards.tolerance(qdot, margin=self.qdot_limit).min()
         vel_reward = (1 + vel_reward) / 2
 
         reward = pos_reward * vel_reward
-        # self.reward_breakup.append([upright.mean(), small_velocity])
+        self.reward_breakup.append([pos_reward, vel_reward])
 
         return reward
 
