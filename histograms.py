7e7d76d45988c69ebd8c8c6f4e6f6ec3a1c97671
Riccardo Sepe <seric814@gmail.com>
2024-01-31 16:54:35 +0100

    Function for joint distribution of q and qdot

--- histograms.py
+++ histograms.py
@@ -19,6 +19,38 @@ def states_histograms(buffer):
     plt.show()
 
 
+def joint_positions_velocities_histograms(buffer):
+    states = np.array([np.array(s[0]) for s in buffer.buffer])
+    if states.shape[1] != 6:
+        raise ValueError(f"States have shape {states.shape} instead of (N, 6)")
+    fig, axs = plt.subplots(3, 1, figsize=(10, 30))
+    for i in range(3):
+        position = states[:, i]
+        velocity = states[:, i + 3]
+        # Create a 2D histogram
+        hist, xedges, yedges = np.histogram2d(position, velocity, bins=10)
+
+        # Plot the 2D histogram as an image on the axis
+        cax = axs[i].imshow(hist, interpolation='nearest', origin='lower',
+                            extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+                            aspect='auto'
+                            )
+
+        # Add colorbar and labels
+        cbar = fig.colorbar(cax, ax=axs[i], fraction=0.05)
+        cbar.set_label('Frequency', fontsize=14)
+        axs[i].set_xlabel('q', fontsize=14)
+        axs[i].set_ylabel('qdot', fontsize=14)
+
+        axs[i].set_title(PLT_LABELS[i], fontsize=16)
+        axs[i].set_aspect(abs(xedges[-1] - xedges[0]) / abs(yedges[-1] - yedges[0]))
+        axs[i].tick_params(axis='both', which='major', labelsize=12)
+
+    plt.tight_layout()
+    fig.suptitle("States joint histograms", fontsize=20)
+    plt.show()
+
+
 def actions_histograms(buffer):
     import matplotlib.pyplot as plt
     actions = np.array([np.array(sample[1]) for sample in buffer.buffer])
