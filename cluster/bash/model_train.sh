f61c2fac9f7877299c6fbd2e90847ff7fdcc6619
Riccardo Sepe <seric814@gmail.com>
2024-02-12 18:58:40 +0100

    Updated model_train.sh script

--- cluster/bash/model_train.sh
+++ cluster/bash/model_train.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
 # if number of cmd arguments is different from 2, print error message and exit
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters"
-    echo "Usage: bash model_train.sh <environment> <seed>"
+    echo "Usage: bash model_train.sh <seed>"
     exit 1
 fi
 
-# first argument must be among the following: acrobot, acro3bot, cartpole, cart2pole, cart3pole, pendulum, reacher
-case $1 in
-    "acrobot" | "acro3bot" | "cartpole"  | "cart2pole" | "cart3pole" | "pendulum" | "reacher" | "soft_reacher")
-        environment=$1;;
-    *)
-        echo "Invalid environment"
-        echo "Usage: bash model_train.sh <environment> <seed>"
-        exit
-        ;;
-esac
-
-
 # second argument must be an integer
-if ! [[ $2 =~ ^[0-9]+$ ]]; then
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
     echo "Error: <seed> must be an integer."
-    echo "Usage: bash model_train.sh <environment> <seed>"
+    echo "Usage: bash model_train.sh <seed>"
     exit 1
 else
     seed=$2
