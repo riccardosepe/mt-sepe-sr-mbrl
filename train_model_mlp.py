558943573af75274ba0eff34a44861a0533c3b65
Riccardo Sepe <seric814@gmail.com>
2024-03-14 15:10:15 +0100

    Adapted to fixed dataset

--- train_model_mlp.py
+++ train_model_mlp.py
@@ -27,19 +27,10 @@ def train_model(resume=False, preprocess=False, seed=None):
     env = SoftReacher(mle=False)
 
     tensorboard_dir = os.path.join(base_dir, "tensorboard")
-    K = 200
-    # K = 10
     lr = 3e-4
-
-    replay_size = 200000
-
     clip_term = 100
 
     batch_size = 64
-
-    # preprocess threshold
-    pth = 0.5
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     writer = SummaryWriter(log_dir=tensorboard_dir)
@@ -57,35 +48,12 @@ def train_model(resume=False, preprocess=False, seed=None):
         reward_model.parameters(), lr=lr)
 
     a_scale = torch.tensor(env.a_scale, dtype=torch.float64, device=device)
+
+    data = torch.load("data/seed_27/data.pt")
+    replay_buffer = data["small"]
+
     if not resume:
-        replay_buffer = ReplayBuffer(replay_size, device)
-        pbar = tqdm(range(replay_size))
-        # Initialize replay buffer with K random episodes
-        for episode in range(K):
-            pbar.set_postfix_str(f"Episode {episode+1}/{K}")
-            o, _, _ = env.reset()
-            o_tensor = torch.tensor(
-                o, dtype=torch.float64, device=device)
-            while True:
-                a = np.random.uniform(-1.0, 1.0, size=env.action_size)
-                o_1, r, done = env.step(a, last=True)
-                a_tensor = torch.tensor(
-                    a, dtype=torch.float64, device=device)
-                o_1_tensor = torch.tensor(
-                    o_1, dtype=torch.float64, device=device)
-                r_tensor = torch.tensor(
-                    r, dtype=torch.float64, device=device)
-                replay_buffer.push(o_tensor, a_tensor, r_tensor, o_1_tensor)
-                pbar.update(1)
-                o_tensor = o_1_tensor
-
-                if done:
-                    break
-            if pbar.n >= replay_size:
-                break
-
         epoch0 = 0
-        print("Done initialization ...")
     else:
         ckpt = torch.load(os.path.join(base_dir, "emergency.ckpt"))
         epoch0 = ckpt['epoch']
@@ -94,8 +62,6 @@ def train_model(resume=False, preprocess=False, seed=None):
 
         transition_optimizer.load_state_dict(ckpt['transition_optimizer'])
         reward_optimizer.load_state_dict(ckpt['reward_optimizer'])
-
-        replay_buffer = ckpt['replay_buffer']
 
         print("Loaded checkpoint")
 
@@ -155,7 +121,6 @@ def train_model(resume=False, preprocess=False, seed=None):
                           'transition_optimizer': transition_optimizer.state_dict(),
                           'reward_model': reward_model.state_dict(),
                           'reward_optimizer': reward_optimizer.state_dict(),
-                          'replay_buffer': replay_buffer
                           }
             torch.save(checkpoint, os.path.join(base_dir, "emergency.ckpt"))
             rollout_plots(env,
