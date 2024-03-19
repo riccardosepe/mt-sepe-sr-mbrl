41af91695a90fe08d2a7cdb6e792928cc2145e5a
Riccardo Sepe <seric814@gmail.com>
2024-03-19 19:21:31 +0100

    Updated code to convert checkpoints

--- convert.py
+++ convert.py
@@ -9,13 +9,14 @@ def main():
     base_path = "/Users/riccardo/PycharmProjects/TUDelft/branches/Physics_Informed_Model_Based_RL/FINAL/model_mlp"
 
     for i in trange(5):
-        base_dir = os.path.join(base_path, f"seed_{i}")
-        file = os.path.join(base_dir, "emergency.ckpt")
-        checkpoint = torch.load(file, map_location=device)
-        smaller_ckpt = {}
-        smaller_ckpt['transition_model'] = checkpoint['transition_model']
-        smaller_ckpt['reward_model'] = checkpoint['reward_model']
-        torch.save(smaller_ckpt, os.path.join(base_dir, "only_model.ckpt"))
+        for lr in ["3e-03", "3e-04", "3e-05"]:
+            base_dir = os.path.join(base_path, f"seed_{i}_lr_{lr}")
+            file = os.path.join(base_dir, "emergency.ckpt")
+            checkpoint = torch.load(file, map_location=device)
+            smaller_ckpt = {}
+            smaller_ckpt['transition_model'] = checkpoint['transition_model']
+            smaller_ckpt['reward_model'] = checkpoint['reward_model']
+            torch.save(smaller_ckpt, os.path.join(base_dir, "only_model.ckpt"))
 
 
 if __name__ == '__main__':
