8fd8da083e3b2a9f3713eb5ae55c0ccfa8ae8919
Riccardo Sepe <seric814@gmail.com>
2024-03-26 16:51:58 +0100

    New plot code

--- visualization/plot_task.py
+++ visualization/plot_task.py
@@ -27,6 +27,79 @@ def plot(save=False):
     data_mlp = np.load(f"{os.path.dirname(__file__)}/../FINAL/eval/data_mlp.npy", allow_pickle=True).item()
     data_lnn = np.load(f"{os.path.dirname(__file__)}/../FINAL/eval/data_lnn.npy", allow_pickle=True).item()
 
+    fig, ax = plt.subplots(nrows=2,
+                           ncols=1,
+                           figsize=(12, 8))
+
+    ee_env = np.array(data_env['ee'][0])
+    ee_mlp = np.array(data_mlp['ee'][0])
+    ee_lnn = np.array(data_lnn['ee'][0])
+
+    goal = data_env['goal']
+
+    colors = [
+        '#1f77b4',
+        '#ff7f0e'
+    ]
+
+    b = ['$x', '$y']
+
+    num_steps = len(ee_env)
+    marker_every = np.arange(0, num_steps-1, 100)
+    # marker_every = list(marker_every)
+    for i in range(2):
+        ax[i].axhline(goal[i],
+                   color=adjust_color_brightness(colors[i], 0.3),
+                   linestyle='--',
+                   label=b[i]+"_{goal}$",
+                   linewidth=7,
+                   alpha=1
+                   )
+
+        ax[i].plot(ee_env[:, i],
+                label=b[i]+"^{ENV}$",
+                linewidth=3,
+                color=adjust_color_brightness(colors[i], -0.15),
+                marker='o',
+                markevery=10+marker_every,
+                markersize=10,
+                alpha=1
+                )
+
+        ax[i].plot(ee_lnn[:, i],
+                label=b[i]+"^{LNN}$",
+                color=adjust_color_brightness(colors[i], -0.5),
+                linestyle='-',
+                # marker='^',
+                # markersize=10,
+                # markevery=50+marker_every,
+                linewidth=3,
+                alpha=1
+                )
+
+        ax[i].plot(ee_mlp[:, i],
+                label=b[i]+"^{MLP}$",
+                color=adjust_color_brightness(colors[i], -0.15),
+                linestyle='-',
+                linewidth=3,
+                alpha=1
+                )
+        ax[i].grid()
+        ax[i].legend(ncol=4)
+
+
+    # fig.legend(loc='upper center', ncol=4)
+    fig.tight_layout()
+    # fig.subplots_adjust(top=0.82)
+
+    plt.show()
+
+
+def plot2(save=False):
+    data_env = np.load(f"{os.path.dirname(__file__)}/../FINAL/eval/data_env.npy", allow_pickle=True).item()
+    data_mlp = np.load(f"{os.path.dirname(__file__)}/../FINAL/eval/data_mlp.npy", allow_pickle=True).item()
+    data_lnn = np.load(f"{os.path.dirname(__file__)}/../FINAL/eval/data_lnn.npy", allow_pickle=True).item()
+
     fig, ax = plt.subplots(figsize=(12, 8))
 
     ee_env = np.array(data_env['ee'][0])
